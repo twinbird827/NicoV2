6fbe2bd12c30a5902a7ab3cef1a1b46b059a75f1
twinbird827 <twinbird827@gmail.com>
2018-04-13 18:43:02 +0900

    todo

---
+++ IO/Constants.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicoV2.IO
+{
+    public class Constants
+    {
+        /**************************************************
+         * 共通
+         **************************************************/
+
+        /// <summary>
+        /// ｱﾌﾟﾘｹｰｼｮﾝ固有のId
+        /// </summary>
+        public const string ApplicationId = "P&imsbQSWKKQ";
+
+        /// <summary>
+        /// 設定ﾌｧｲﾙのﾊﾟｽ
+        /// </summary>
+        public const string AppConfig = @".\lib\app-config.ini";
+
+        /// <summary>
+        /// 設定ﾌｧｲﾙのﾃﾞﾌｫﾙﾄｾｸｼｮﾝ
+        /// </summary>
+        public const string AppConfigDefaultSection = "NicoV2";
+
+        /**************************************************
+         * HTTPS関連の定数
+         **************************************************/
+
+        /// <summary>
+        /// ﾛｸﾞｲﾝUrl
+        /// </summary>
+        public const string LoginUrl = "https://secure.nicovideo.jp/secure/login";
+
+        /// <summary>
+        /// ﾛｸﾞｲﾝﾊﾟﾗﾒｰﾀ
+        /// </summary>
+        public const string LoginParameter = "site=niconico&mail={0}&password={1}&next_url=http://flapi.nicovideo.jp/api/getflv/sm9";
+
+        /// <summary>
+        /// ﾀｲﾑｱｳﾄ
+        /// </summary>
+        public const int Timeout = 100000;
+
+        /// <summary>
+        /// ﾕｰｻﾞｴｰｼﾞｪﾝﾄ
+        /// </summary>
+        public const string UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0";
+
+        /// <summary>
+        /// ﾘﾌｧﾗ
+        /// </summary>
+        public const string Referer = "http://www.nicovideo.jp/";
+
+        /// <summary>
+        /// ｸｯｷｰUrl
+        /// </summary>
+        public const string CookieUrl = "http://nicovideo.jp/";
+
+        /// <summary>
+        /// ｸｯｷｰﾃﾞｰﾀ
+        /// </summary>
+        public const string CookieData = "{0}; expires = {1}";
+
+        /// <summary>
+        /// ﾏｲﾘｽﾄUrl
+        /// </summary>
+        public const string MylistUrl = "http://www.nicovideo.jp/mylist/";
+
+        /// <summary>
+        /// ﾏｲﾘｽﾄﾃﾞﾌｫﾙﾄ
+        /// </summary>
+        public const string MylistDefault = "http://www.nicovideo.jp/api/deflist/list";
+
+        /**************************************************
+         * 検索処理関連の定数
+         **************************************************/
+
+        public const string TargetTag = "tagsExact";
+
+        public const string TargetKeyword = "title,description,tags";
+
+        public const string Fields = "contentId,title,description,tags,categoryTags,viewCounter,mylistCounter,commentCounter,startTime,lastCommentTime,lengthSeconds,thumbnailUrl,communityIcon";
+
+        public const string Context = "kaz.server-on.net/NicoV2";
+
+        public const string Url = "http://api.search.nicovideo.jp/api/v2/video/contents/search?q={0}&targets={1}&fields={2}&_sort={3}&_offset={4}&_limit={5}&_context={6}";
+
+    }
+}
---
+++ IO/Variables.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicoV2.IO
+{
+    public class Variables : Ini
+    {
+        private static Variables Instance { get; set; } = new Variables();
+
+        protected Variables() : base(Constants.AppConfig, Constants.AppConfigDefaultSection)
+        {
+        }
+
+        public static string MailAddress {
+            get
+            {
+                var tmp = Instance["MAIL_ADDRESS"];
+                return 
+            }
+            set
+            {
+            }
+        }
+        public static string Password { get; set; }
+    }
+}
